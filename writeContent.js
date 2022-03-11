51484b392f9606bf3d53f3c53203477747c6e608
rderickson88 <erickryanson@gmail.com>
2022-03-11 11:27:09 -0500

    Huge breakthrough on the DOM side

--- writeContent.js
+++ writeContent.js
@@ -1,6 +1,20 @@
 
 
-            
+myData = [
+
+    {"Title": "Candy Maze - Ebbe", 
+    "Sum": "This is a unique 'Pac-Man' style game that was built for up to 2 players to collect various candy pieces on a variety of maps.", 
+    "Editor": "This is Mr. E's Commentary", 
+    "Links": "/Games/ryanGame/index.html"},
+
+    {"Title": "Rocket Game - Xavier", 
+    "Sum": "A classic platformer with a unique twist. Use your reloading rocket launcher to not only take care of enemies but to also propel yourself through challenging platforming tests.", 
+    "Editor": "This is Mr. E's Commentary", 
+    "Links": "/Games/ryanGame/index.html"},
+
+   
+
+]
 
 
 
@@ -8,10 +22,12 @@
    
 
 function callGameContent(gameNum) {
-    const myGameText = JSON.parse(gameContent);
-    const newP = document.createElement("p");
-    const newContent = document.createTextNode(myGameText[0]);
-    newP.appendChild(newContent);
-    document.getElementById("gameContent").appendChild(newP);
+
+    document.getElementById("gameTitle").innerHTML = myData[gameNum].Title;
+    document.getElementById("gameLink").innerHTML = "Play the Game!";
+    document.getElementById("gameLink").setAttribute('href', myData[gameNum].Links);
+    document.getElementById("gameContent").innerHTML = myData[gameNum].Sum;
+    document.getElementById("editorTitle").innerHTML = "Mr. E's Commentary";
+    document.getElementById("gameEditor").innerHTML = myData[gameNum].Editor;
 
 }
